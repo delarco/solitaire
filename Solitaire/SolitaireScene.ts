6e9e845bc1db885efd7839922ccfd06eb8416fdd
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-01 17:32:00 -0300

    Update solitaire scene

--- Solitaire/SolitaireScene.ts
+++ Solitaire/SolitaireScene.ts
@@ -1,5 +1,30 @@
+import { ExpoWebGLRenderingContext } from "expo-gl";
 import { Scene } from "../Engine/Scene";
+import { ISize } from "../Engine/interfaces/ISize";
+import { ShaderInfo, ShaderType } from "../Engine/webgl/ShaderInfo";
+import { vertexShaderSourceCode } from "./shaders/VertexShader";
+import { fragmentShaderSourceCode } from "./shaders/FragmentShader";
 
 export class SolitaireScene extends Scene {
-    
+
+    constructor(
+        protected gl: ExpoWebGLRenderingContext,
+        protected screen: ISize,
+    ) {
+        const shaders = [
+            new ShaderInfo(ShaderType.VERTEX_SHADER, vertexShaderSourceCode),
+            new ShaderInfo(ShaderType.FRAGMENT_SHADER, fragmentShaderSourceCode),
+        ]
+        super(gl, screen, shaders)
+    }
+
+    public override async init(): Promise<void> {
+        console.log("[SolitaireScene] init");
+        console.log(this.program);
+        
+    }
+
+    public override update(): void {
+        console.log("[SolitaireScene] update");
+    }
 }

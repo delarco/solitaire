27a2949fb884e7272453d5ba9e5b2df13dc6d207
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-01 16:46:58 -0300

    Create shader program class

---
+++ Engine/webgl/ShaderProgram.ts
@@ -0,0 +1,41 @@
+import { ExpoWebGLRenderingContext } from "expo-gl";
+import { ShaderInfo } from "./ShaderInfo";
+
+export class ShaderProgram {
+
+    public static build(gl: ExpoWebGLRenderingContext, shadersInfo: Array<ShaderInfo>): WebGLProgram | null {
+
+        const program = gl.createProgram();
+
+        if (!program) throw new Error("Can't create WebGLProgram")
+
+        for (const shaderInfo of shadersInfo) {
+            const shader = ShaderProgram.compileShader(gl, shaderInfo);
+            if (shader) gl.attachShader(program, shader);
+        }
+
+        gl.linkProgram(program);
+
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            throw new Error(`Error linking shader program: ${gl.getProgramInfoLog(program)}`)
+        }
+
+        return program;
+    }
+
+    private static compileShader(gl: ExpoWebGLRenderingContext, shaderInfo: ShaderInfo): WebGLShader | null {
+
+        const shader = gl.createShader(shaderInfo.type);
+
+        if (!shader) throw new Error("Can't create WebGLShader")
+
+        gl.shaderSource(shader, shaderInfo.sourceCode);
+        gl.compileShader(shader);
+
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            throw new Error(`Error compiling ${shaderInfo.type} shader: ${gl.getShaderInfoLog(shader)}`)
+        }
+
+        return shader;
+    }
+}

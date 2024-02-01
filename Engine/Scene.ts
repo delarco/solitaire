3abd65ebd75270441577b06d9cc8a797ad9ef8ba
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-01 17:31:18 -0300

    Add program and objects to scene

--- Engine/Scene.ts
+++ Engine/Scene.ts
@@ -1,22 +1,28 @@
 import { ExpoWebGLRenderingContext } from "expo-gl";
 import { ISize } from "./interfaces/ISize";
+import { GameObject } from "./GameObject";
+import { ShaderProgram } from "./webgl/ShaderProgram";
+import { ShaderInfo } from "./webgl/ShaderInfo";
 
 export class Scene {
+
+    public program: ShaderProgram
+    public objects: Array<GameObject> = []
 
     constructor(
         protected gl: ExpoWebGLRenderingContext,
         protected screen: ISize,
-    ) { }
+        shadersInfo: Array<ShaderInfo> = []
+    ) {
 
-    public async preload(): Promise<void> {
-        console.log("[Scene] preload");
+        this.program = new ShaderProgram(gl, shadersInfo)
     }
 
     public async init(): Promise<void> {
-        console.log("[Scene] init");
+        throw new Error("[Scene] init")
     }
 
-    public update(time: number): void {
-        console.log("[Scene] update");
+    public update(time: number, deltaTime: number): void {
+        throw new Error("[Scene] update")
     }
 }

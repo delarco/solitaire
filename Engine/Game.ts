8617f9b65f8efef13d313ccd63d4ad6549cfb8d7
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-01 17:31:51 -0300

    Implement game class basic functionality

--- Engine/Game.ts
+++ Engine/Game.ts
@@ -5,6 +5,11 @@ import { ISize } from "./interfaces/ISize";
 
 export class Game {
 
+    private renderingScenes: Array<Scene> = []
+
+    private fps: number = 0
+    private lastTime = 0
+
     constructor(
         private gl: ExpoWebGLRenderingContext,
         private screenSize: ISize,
@@ -17,5 +22,37 @@ export class Game {
     public async start(): Promise<void> {
 
         console.log("[Game] start")
+
+        if (this.scenes.length > 0) {
+
+            const SceneType = this.scenes[0]
+            const scene = new SceneType(this.gl, this.screenSize)
+            await scene.init()
+            this.renderingScenes.push(scene)
+        }
+
+        this.animate()
+    }
+
+    private animate(): void {
+
+        for (const scene of this.renderingScenes) {
+            for (const obj of scene.objects) obj.draw()
+        }
+
+        requestAnimationFrame(time => {
+            this.update(time)
+            this.animate()
+        });
+    }
+
+    private update(time: number): void {
+
+        const now = time * 0.001
+        const deltaTime = now - this.lastTime
+        this.lastTime = now
+        this.fps = 1 / deltaTime
+
+        for (const scene of this.renderingScenes) scene.update(time, deltaTime)
     }
 }

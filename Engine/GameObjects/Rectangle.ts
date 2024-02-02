12fee7d36c2ddf037cd73c205328e522a4101fa7
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-01 21:53:08 -0300

    Refactor rectangle to use textures

--- Engine/GameObjects/Rectangle.ts
+++ Engine/GameObjects/Rectangle.ts
@@ -3,10 +3,11 @@ import { ShaderProgram } from "../webgl/ShaderProgram";
 import { Color } from "../Color";
 import { IGameObject } from "../interfaces/IGameObject";
 import { Texture } from "../Texture";
+import { TextureManager } from "../TextureManager";
 
 export class Rectangle implements IGameObject {
 
-    private vertexArray: Float32Array
+    private vertexArray: Float32Array = new Float32Array()
     private vertexBuffer: WebGLBuffer
     private vertexNumComponents: number = 0
     private vertexCount: number = 0
@@ -14,9 +15,10 @@ export class Rectangle implements IGameObject {
     public visible = true
     public draggable = false
 
-
     private _texture: Texture | null = null
-    private textureBuffer: WebGLBuffer | null = null
+    private textureBuffer: WebGLBuffer
+    public get texture() { return this._texture }
+    public set texture(value: Texture | null) { this._texture = value }
 
     constructor(
         private gl: ExpoWebGLRenderingContext,
@@ -29,12 +31,16 @@ export class Rectangle implements IGameObject {
     ) {
 
         const vertexBuffer = gl.createBuffer()
-
         if (!vertexBuffer) throw new Error("Can't create WebGLBuffer (vertex)")
 
         this.vertexBuffer = vertexBuffer
-        this.vertexArray = new Float32Array()
         this.updateVertexArrayBuffer()
+
+        const textureBuffer = gl.createBuffer()
+        if (!textureBuffer) throw new Error("Can't create WebGLBuffer (texture)")
+
+        this.textureBuffer = textureBuffer
+        this.createTextureArrayBuffer()
     }
 
     private updateVertexArrayBuffer(): void {
@@ -55,39 +61,7 @@ export class Rectangle implements IGameObject {
         this.vertexCount = this.vertexArray.length / this.vertexNumComponents;
     }
 
-    public draw(program: ShaderProgram): void {
-
-        if (!this.visible) return
-
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer)
-        this.gl.vertexAttribPointer(program.vertexPosition, this.vertexNumComponents, this.gl.FLOAT, false, 0, 0);
-
-        this.gl.uniform4fv(program.colorLocation, this.color.array);
-        this.gl.drawArrays(this.gl.TRIANGLES, 0, this.vertexCount);
-    }
-
-    public move(x: number | null = null, y: number | null = null, z: number | null = null): void {
-
-        if (x) this.x = x
-        if (y) this.y = y
-        if (z) this.z = z
-
-        this.updateVertexArrayBuffer()
-    }
-
-    public get texture() {
-
-        return this._texture
-    }
-
-    public set texture(value: Texture | null) {
-
-        this._texture = value
-
-        if (this.textureBuffer) return
-
-        this.textureBuffer = this.gl.createBuffer()
-        if (!this.textureBuffer) throw new Error("Can't create WebGLBuffer (texture)")
+    private createTextureArrayBuffer(): void {
 
         const textureArray = new Float32Array([
             0, 0,
@@ -100,5 +74,30 @@ export class Rectangle implements IGameObject {
 
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.textureBuffer);
         this.gl.bufferData(this.gl.ARRAY_BUFFER, textureArray, this.gl.STATIC_DRAW);
+    }
+
+    public draw(program: ShaderProgram): void {
+
+        if (!this.visible) return
+
+        this.gl.uniform1i(program.textureLocation, this._texture?.id ?? TextureManager.BLANK_TEXTURE.id)
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer)
+        this.gl.vertexAttribPointer(program.vertexPosition, this.vertexNumComponents, this.gl.FLOAT, false, 0, 0);
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.textureBuffer);
+        this.gl.vertexAttribPointer(program.textureCoord, 2, this.gl.FLOAT, false, 0, 0);
+
+        this.gl.uniform4fv(program.colorLocation, this.color.array);
+        this.gl.drawArrays(this.gl.TRIANGLES, 0, this.vertexCount);
+    }
+
+    public move(x: number | null = null, y: number | null = null, z: number | null = null): void {
+
+        if (x) this.x = x
+        if (y) this.y = y
+        if (z) this.z = z
+
+        this.updateVertexArrayBuffer()
     }
 }

44d89fc53f2aeeba76cec9acb5535366ce3ceb35
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-03 17:22:33 -0300

    Create stock clear action

---
+++ Solitaire/Actions/StockClearAction.ts
@@ -0,0 +1,12 @@
+import { IAction } from "../interfaces/IAction";
+
+// TODO: implement StockClearAction
+export class StockClearAction implements IAction {
+
+    execute(): void {
+        throw new Error("Method not implemented.");
+    }
+    undo(): void {
+        throw new Error("Method not implemented.");
+    }
+}

eb196959ab3236d6421d76fb2c9c2b4d5efd62bd
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-03 14:46:44 -0300

    Refactor dimensions

--- Solitaire/Utils/Dimensions.ts
+++ Solitaire/Utils/Dimensions.ts
@@ -12,6 +12,7 @@ export class Dimensions {
     public static cardVerticalMargin: number
 
     public static buttonSize: ISize = { width: 0, height: 0 }
+    public static buttonsY: number
 
     public static foundationPilesY: number
     public static tableauPilesY: number
@@ -20,17 +21,18 @@ export class Dimensions {
 
         Dimensions.screenSize = screen
         Dimensions.screenPaddingTop = Math.floor(screen.height * 0.1)
-        Dimensions.screenPaddingBottom = Math.floor(screen.height * 0.05)
+        Dimensions.screenPaddingBottom = Math.floor(screen.height * 0.1)
 
         const screenWidthOver8 = Math.floor(screen.width / 8)
         Dimensions.cardSize.width = screenWidthOver8
         Dimensions.cardSize.height = Math.floor(Dimensions.cardSize.width * 1.4)
         Dimensions.cardVerticalMargin = Math.ceil(Dimensions.cardSize.height * 0.3)
-        
+
         Dimensions.gapBetweenPiles = Math.floor(screenWidthOver8 / 8)
 
         Dimensions.buttonSize.width = Dimensions.cardSize.width
         Dimensions.buttonSize.height = Dimensions.buttonSize.width
+        Dimensions.buttonsY = screen.height - Dimensions.screenPaddingBottom - this.buttonSize.height
 
         Dimensions.foundationPilesY = Dimensions.screenPaddingTop
         Dimensions.tableauPilesY = Dimensions.foundationPilesY + Dimensions.cardSize.height + Dimensions.gapBetweenPiles
@@ -44,11 +46,11 @@ export class Dimensions {
 
         console.log("[Dimensions] cardSize", Dimensions.cardSize.width, Dimensions.cardSize.height)
         console.log("[Dimensions] cardVerticalMargin", Dimensions.cardVerticalMargin)
-        
+
         console.log("[Dimensions] gapBetweenPiles", Dimensions.gapBetweenPiles)
 
         console.log("[Dimensions] buttonSize", Dimensions.buttonSize.width, Dimensions.buttonSize.height)
-        
+
         console.log("[Dimensions] foundationPilesY", Dimensions.foundationPilesY)
         console.log("[Dimensions] tableauPilesY", Dimensions.tableauPilesY)
     }

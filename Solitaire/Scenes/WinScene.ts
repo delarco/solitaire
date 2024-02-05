202d7c14616a7cdaaea16eb2847e4dd5f03abf6a
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-05 03:30:14 -0300

    Refactor win scene

--- Solitaire/Scenes/WinScene.ts
+++ Solitaire/Scenes/WinScene.ts
@@ -2,6 +2,7 @@ import { Animator } from "../../Engine/Animations/Animator";
 import { ColorBlinkAnimation } from "../../Engine/Animations/ColorBlinkAnimation";
 import { Color } from "../../Engine/Color";
 import { Rectangle } from "../../Engine/GameObjects/Rectangle";
+import { Window } from "../../Engine/GameObjects/Window";
 import { Scene } from "../../Engine/Scene";
 import { TextureManager } from "../../Engine/TextureManager";
 import { IGameObject } from "../../Engine/interfaces/IGameObject";
@@ -14,14 +15,20 @@ import { Dimensions } from "../Utils/Dimensions";
 
 export class WinScene extends Scene {
 
-    private congratulation!: Text
-    private background!: Rectangle
-    private newGameButton!: Text
+    private readonly CONGRATULATIONS_TEXT = "CONGRATULATIONS!"
+    private readonly PLACEHOLDER_TEXT = "            "
+    private readonly MOVES_TEXT = "MOVES:   135"
+    private readonly TIME_TEXT = "TIME:   2:47"
+    private readonly SCORE_TEXT = "SCORE:  6530"
+    private readonly RECORD_TEXT = "RECORD: 7680"
+    private readonly NEW_GAME_TEXT = "NEW GAME"
 
-    private moves!: Text
-    private time!: Text
-    private score!: Text
-    private record!: Text
+    // private congratulation!: Text
+    // private newGameButton!: Text
+    // private moves!: Text
+    // private time!: Text
+    // private score!: Text
+    // private record!: Text
 
     public onNewGamePress: (() => void) | null = null
 
@@ -35,71 +42,55 @@ export class WinScene extends Scene {
 
     public async init(): Promise<void> {
 
-        const screen80: ISize = {
-            width: Math.floor(Dimensions.screenSize.width * 0.8),
-            height: Math.floor(Dimensions.screenSize.height * 0.8)
-        }
-
-        const screen70: ISize = {
-            width: Math.floor(Dimensions.screenSize.width * 0.7),
-            height: Math.floor(Dimensions.screenSize.width * 0.7),
-        }
-
-        this.background = new Rectangle("background",
-            (Dimensions.screenSize.width - screen80.width) / 2,
-            ((Dimensions.screenSize.height - screen70.height) / 10) * 3,
-            0,
-            screen80.width,
-            screen70.height,
-            new Color(0x2d / 255, 0x7b / 255, 0x40 / 255)
-        )
-        this.background.texture = TextureManager.getTexture("card-flipped")
-        
         const whiteFontTexture = TextureManager.getTexture("white-font")!
         const yellowFontTexture = TextureManager.getTexture("yellow-font")!
 
-        this.congratulation = new Text("CONGRATULATIONS!", 0, 0, 1, 32, yellowFontTexture)
-        this.newGameButton = new Text("NEW GAME", 0, 200, 2, 48, yellowFontTexture)
-        this.moves = new Text("MOVES:  1", 0, 0, 1, 32, whiteFontTexture)
-        this.time = new Text("TIME:   2", 0, 0, 1, 32, whiteFontTexture)
-        this.score = new Text("SCORE:  3", 0, 0, 1, 32, whiteFontTexture)
-        this.record = new Text("RECORD: 4", 0, 0, 1, 32, whiteFontTexture)
 
-        this.congratulation.move(
-            (Dimensions.screenSize.width - this.congratulation.width) / 2,
-            ((Dimensions.screenSize.height - screen70.height) / 10) * 4
+        const windowPosition = Dimensions.centerPosition(Dimensions.screenSize80, Dimensions.screenSize)
+
+        const winWindow = new Window("win-window",
+            windowPosition.x, windowPosition.y, 100,
+            Dimensions.screenSize80.width, Dimensions.screenSize50.height
         )
 
-        this.newGameButton.move(
-            (Dimensions.screenSize.width - this.newGameButton.width) / 2,
-            this.background.y + this.background.height - (this.newGameButton.height * 3),
-        )
+        const congratulationsFontSize = Dimensions.fontSizeToFitWidth(this.CONGRATULATIONS_TEXT, winWindow.width * 0.9)
+        const congratulationsSize = Dimensions.textSize(this.CONGRATULATIONS_TEXT, congratulationsFontSize)
+        const congratulationsPosition = Dimensions.centerPosition(congratulationsSize, winWindow)
+        const congratulationText = new Text(this.CONGRATULATIONS_TEXT, congratulationsPosition.x, congratulationsSize.height, 1, congratulationsFontSize, yellowFontTexture)
 
-        this.newGameButton.onPress = () => {
-            if (this.onNewGamePress) this.onNewGamePress()
-        }
+        const newGameFontSize = Dimensions.fontSizeToFitWidth(this.NEW_GAME_TEXT, winWindow.width * 0.7)
+        const newGameSize = Dimensions.textSize(this.NEW_GAME_TEXT, newGameFontSize)
+        const newGamePosition = Dimensions.centerPosition(newGameSize, winWindow)
+        const newGameButton = new Text(this.NEW_GAME_TEXT, newGamePosition.x, winWindow.height - 2 * newGameSize.height, 2, newGameFontSize, yellowFontTexture)
 
-        this.moves.move(this.congratulation.x, this.congratulation.y + (this.congratulation.height * 2))
-        this.time.move(this.congratulation.x, this.moves.y + (this.moves.height * 1.5))
-        this.score.move(this.congratulation.x, this.time.y + (this.time.height * 1.5))
-        this.record.move(this.congratulation.x, this.score.y + (this.score.height * 1.5))
+        const dataFontSize = Dimensions.fontSizeToFitWidth(this.PLACEHOLDER_TEXT, winWindow.width * 0.7)
+        const dataSize = Dimensions.textSize(this.PLACEHOLDER_TEXT, dataFontSize)
+        const dataPosition = Dimensions.centerPosition(dataSize, winWindow)
+        dataPosition.y = congratulationText.y + congratulationText.height + dataSize.height
 
-        this.objects.push(this.background)
-        this.objects.push(this.congratulation)
-        this.objects.push(this.moves)
-        this.objects.push(this.time)
-        this.objects.push(this.score)
-        this.objects.push(this.record)
-        this.objects.push(this.newGameButton)
+        const movesText = new Text(this.MOVES_TEXT, dataPosition.x, dataPosition.y + (0 * (dataSize.height * 1.5)), 2, dataFontSize, whiteFontTexture)
+        const timeText = new Text(this.TIME_TEXT, dataPosition.x, dataPosition.y + (1 * (dataSize.height * 1.5)), 2, dataFontSize, whiteFontTexture)
+        const scoreText = new Text(this.SCORE_TEXT, dataPosition.x, dataPosition.y + (2 * (dataSize.height * 1.5)), 2, dataFontSize, whiteFontTexture)
+        const recordText = new Text(this.RECORD_TEXT, dataPosition.x, dataPosition.y + (3 * (dataSize.height * 1.5)), 2, dataFontSize, whiteFontTexture)
+
+        winWindow.addObject(congratulationText)
+        winWindow.addObject(movesText)
+        winWindow.addObject(timeText)
+        winWindow.addObject(scoreText)
+        winWindow.addObject(recordText)
+        winWindow.addObject(newGameButton)
+        this.objects.push(winWindow)
     }
 
     public update(time: number, deltaTime: number): void { }
 
     public onGameObjectTouchStart(gameObject: IGameObject): void {
 
-        if (gameObject === this.newGameButton) {
+        if (this.onNewGamePress) this.onNewGamePress()
 
-            Animator.add(new ColorBlinkAnimation(gameObject))
-        }
+        // if (gameObject === this.newGameButton) {
+
+        //     Animator.add(new ColorBlinkAnimation(gameObject))
+        // }
     }
 }
